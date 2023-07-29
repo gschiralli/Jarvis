dda343d2fd1378286e9168dcc2fddca095de0aae
Gianni Schiralli <gschiralli@myseneca.ca>
2023-07-29 14:27:17 -0400

    Added clerk auth

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface pageProps {}
+
+const LandingPage: FC<pageProps> = ({}) => {
+  return <div>Landing Page (Unprotected)</div>;
+};
+
+export default LandingPage;
