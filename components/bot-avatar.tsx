af5f3585b294350bd75a31c1fdf03156c69ba131
Gianni Schiralli <gschiralli@hotmail.com>
2023-08-03 16:05:16 -0400

    Completed conversation route

---
+++ components/bot-avatar.tsx
@@ -0,0 +1,12 @@
+import { useUser } from "@clerk/nextjs";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+export const BotAvatar = () => {
+  const { user } = useUser();
+
+  return (
+    <Avatar className="h-8 w-8">
+      <AvatarImage src="/logo.png" />
+    </Avatar>
+  );
+};
